8e17679a3e28bf7b014a2743d25083997a204245
NirajAcharya-online <nirajacharyaonline@gmail.com>
2026-04-16 22:30:42 +0545

    Addb Usser model

---
+++ src/user/userModel.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import type { IUser } from './userTypes.js';
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model<IUser>('User', userSchema);
