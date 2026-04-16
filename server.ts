cd9130ecb989dce88ff8e3552744be46acb019bd
NirajAcharya-online <nirajacharyaonline@gmail.com>
2026-04-16 16:07:57 +0545

    Expresss setup

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-console.log("Hello World From Outside the World!");
+import app from './src/app.js';
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`App listing on Port ${3000}`);
+  });
+};
+startServer();
