2bb58094acd4fa647ddc5119850c64aba3c52c40
NirajAcharya-online <nirajacharyaonline@gmail.com>
2026-04-17 09:36:36 +0545

    Implemnting json web token

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,6 @@ const _config = {
   port: Number(process.env.PORT),
   databaseUri: String(process.env.MONGO_DB_URI),
   env: String(process.env.NODE_ENV),
+  jwtSecret: String(process.env.JWT_SECRET),
 };
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import type { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
 import { User } from './userModel.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config.js';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -16,7 +18,18 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ sub: newUser._id.toString() }, config.jwtSecret, {
+    expiresIn: '7d',
+  });
+
   return res.status(200).json({
+    accessToken: token,
     message: 'User registered Sucessfully!',
   });
 };
